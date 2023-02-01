50f26ebd1384cf7766b9a68d0987949efb3c2acb
Ankit Mahato <ankmahato@gmail.com>
2023-02-01 14:31:51 +0530

    intital setup

---
+++ setup.py
@@ -0,0 +1,50 @@
+from os import path, walk
+from setuptools import setup
+from importlib.machinery import SourceFileLoader
+
+### To run the tests:
+# python setup.py test
+
+### To install on a machine:
+# sudo python setup.py install
+
+### To install in a home directory (~/lib):
+# python setup.py install --home=~
+LONG_DESCRIPTION = open(path.join(path.dirname(__file__), 'README.md')).read()
+
+# avoid loading the package before requirements are installed:
+version = SourceFileLoader('version', 'foss42/version.py').load_module()
+
+DEPENDENCIES = ["Unidecode",
+                "titlecase",
+                "typeguard",
+                ]
+
+setup(name="foss42",
+      version=str(version.VERSION),
+      author="Ankit Mahato",
+      author_email="ankmahato@gmail.com",
+      keywords='foss42 utility coversion validation generate transform ml bi visualization charts language graphs data map',
+      description="Core Python library used by foss42 APIs.",
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type="text/markdown",
+      url="https://github.com/foss42/foss42",
+      project_urls={
+            "Homepage": "https://foss42.com",
+            "Source Code": "https://github.com/foss42/foss42",
+            "Bug Tracker": "https://github.com/foss42/foss42",
+            "Documentation": "https://github.com/foss42/foss42/issues",
+      },
+      test_suite="test",
+      packages=['foss42'],
+      install_requires=DEPENDENCIES,
+      tests_require=DEPENDENCIES,
+      python_requires='>=3.10',
+      classifiers=[  
+            'Programming Language :: Python :: 3 :: Only',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
+            'Natural Language :: English',
+      ],      
+      )
